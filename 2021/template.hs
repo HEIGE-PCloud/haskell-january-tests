829b8c819719cc0d2e4ef9716b651dd48791ea6c
HEIGE-PCloud <heige.pcloud@outlook.com>
2023-01-05 20:08:05 +0000

    2021: implement part 1

--- 2021/template.hs
+++ 2021/template.hs
@@ -11,21 +11,36 @@ import Examples
 -- Part I
 --
 count :: Eq a => a -> [a] -> Int
-count 
-  = undefined
+count x ys
+  = sum [1 | y <- ys, x == y]
 
 degrees :: Eq a => Graph a -> [(a, Int)]
-degrees 
-  = undefined
+degrees ([], _)
+  = []
+degrees ((n : ns), es)
+  = (n, (x + y)) : degrees (ns, es)
+    where
+      x = count n (map fst es) 
+      y = count n (map snd es)
+      
+-- return the other item in the pair
+-- other (1, 2) 1 -> 2
+-- other (1, 2) 2 -> 1
+other :: Eq a => (a, a) -> a -> a
+other (x, y) z
+  | x == z = y
+  | otherwise = x
 
 neighbours :: Eq a => a -> Graph a -> [a]
-neighbours 
-  = undefined
+neighbours n (_, es)
+  = [other e n | e@(n1, n2) <- es, n == n1 || n == n2]
 
 removeNode :: Eq a => a -> Graph a -> Graph a
-removeNode 
-  = undefined
-
+removeNode n (ns, es)
+  = (ns', es')
+  where
+    ns' = filter (/= n) ns
+    es' = filter ((/= n) . snd) (filter ((/= n) . fst) es)
 ------------------------------------------------------
 --
 -- Part II
